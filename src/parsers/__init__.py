2035610dd2b2114c723b56b87954ea26f64a76a0
Konstantin-create <schahov.konst@gmail.com>
2023-01-02 17:18:57 +0300

    feat: generate subparser

---
+++ src/parsers/__init__.py
@@ -0,0 +1,1 @@
+from .generate_parser import *
---
+++ src/parsers/generate_parser.py
@@ -0,0 +1,20 @@
+from .parser import subparsers
+from argparse import Namespace
+
+
+def generate_handler(args: Namespace):
+    """Handler for generate command"""
+
+    if args.source_all:
+        print('All')
+   
+
+# Generate parser
+generate_parser = subparsers.add_parser('generate', help='Command to generate random idea from source')
+
+generate_parser.add_argument(
+    '-A', '--all',
+    dest='source_all',
+    action='store_true',
+    help='command to generate idea from all source'
+)
