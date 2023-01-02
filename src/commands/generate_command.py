03e770ee51f6138735866e1d0a97b610b26cf5e9
Konstantin-create <schahov.konst@gmail.com>
2023-01-02 18:43:03 +0300

    feat: generate command class

---
+++ src/commands/generate_command.py
@@ -0,0 +1,28 @@
+import random
+from rich import print
+from tools.parse_ideas import *
+from tools.tools import ParseResponce
+
+
+class GenerateCommand:
+    def __init__(self, source_all: bool = True, source: str = ''):
+        self.source_all = source_all
+        self.source = source
+
+    def generate_idea(self) -> ParseResponce:
+        """Function to generate list of ideas from different services"""
+
+        ideas = [parse_w_t_c()]
+        return random.choice(ideas)
+
+    def print_idea(self):
+        """Function to print idea"""
+
+        idea = self.generate_idea()
+        print(
+            f"""
+            [green]Idea were generated![/green]
+            [yellow]{idea.header}[/yellow]
+            {idea.body}
+            """
+        )
