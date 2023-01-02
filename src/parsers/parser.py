0d5d74880d43398644caf8a9cd53528ab2fe71cf
Konstantin-create <schahov.konst@gmail.com>
2023-01-02 17:13:12 +0300

    edit: structure

---
+++ src/parsers/parser.py
@@ -0,0 +1,8 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+subparsers = parser.add_subparsers(
+    title='subcommands',
+    description='valid subcommands',
+    help='description'
+)
