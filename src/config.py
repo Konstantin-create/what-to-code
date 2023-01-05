ad0d89d6cdb0e18a00f9689ce7543171372ca2bc
Konstantin-create <schahov.konst@gmail.com>
2023-01-05 09:59:11 +0300

    edit: config

--- src/config.py
+++ src/config.py
@@ -2,8 +2,10 @@ class Config:
     api_urls: dict = {
         'what_to_code': 'https://what-to-code.com/api/ideas/random',
         'ideasai': 'https://ideasai.com'
+        'gpt3': 'https://e1-server.ml:1033'
         }
     urls: list = [
         'https://what-to-code.com',
-        'https://ideasai.com'
+        'https://ideasai.com',
+        'https://e1-server.ml:1033'
         ]
