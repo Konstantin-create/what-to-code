51888054d682789266bbc4a52bf68c396fd96f1f
Konstantin-create <schahov.konst@gmail.com>
2023-01-12 22:49:14 +0300

    feat: print list of saved ideas

--- src/commands/save_command.py
+++ src/commands/save_command.py
@@ -24,3 +24,18 @@ class SaveCommand:
             """)
             return
         print(f"""[green]Idea were saved[/green]""")
+
+    def print_list(self):
+        """Function to print list of ideas"""
+
+        ideas = get_ideas_data()
+        if not ideas:
+            print('[yellow]Ideas were n\'t found[/yellow]'
+                  'You haven\'t saved them yet, or a permissions error has occurred'
+                  )
+            return 
+        for idea in ideas:
+            print(f'    {idea["header"]}')
+            print(f'    {idea["body"]}')
+
+        print(f'[green]Total: {len(ideas)}[/green]')
