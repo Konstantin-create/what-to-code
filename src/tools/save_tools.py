d767a4dc728d92e58c89c8326860559c9bb4f3f0
Konstantin-create <schahov.konst@gmail.com>
2023-01-12 22:18:27 +0300

    feat: get config data function

--- src/tools/save_tools.py
+++ src/tools/save_tools.py
@@ -5,24 +5,32 @@ import json
 from config import Config
 
 user_path = os.path.expanduser("~")
+default_config_data = {
+    'save_path': '~/.what-to-code'
+}
 
 
 def create_base_structure() -> Union[True, Exception]:
     """Function to create base structure of .what-to-code folder"""
 
-    config_data = {
-        'save_path': '~/.what-to-code'
-    }
-
     try:
         if not os.path.exists('~/.what-to-code'):
             os.mkdir('~/.what-to-code')
         if not os.path.exists(f'{user_path}/.what-to-code/config.json'):
-            json.dump(open(f'{user_path}/.what-to-code/config.json', 'w'), config_data)
+            json.dump(open(f'{user_path}/.what-to-code/config.json', 'w'), default_config_data)
         if not os.path.exists(f'{user_path}/.what-to-code/ideas.json'):
             json.dump(open(f'{user_path}/.what-to-code/ideas.json', 'w'), [])
     except Exception as e:
         return e
+
+
+def get_config_data() -> dict:
+    """Function to get config data from .what-to-code/config.json"""
+
+    try:
+        return json.load(open(f'{user_path}/.what-to-code/config.json', 'r'))
+    except:
+        return default_config_data
 
 
 def save_idea(header: str, body: str) -> Union[True, Exception]:
