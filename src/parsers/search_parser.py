20a52a44d8d98f9b1ffc0b856a2bc2661cc2c88c
Konstantin-create <87200874+Konstantin-create@users.noreply.github.com>
2023-01-13 22:27:31 +0300

    feat: search parser

--- src/parsers/search_parser.py
+++ src/parsers/search_parser.py
@@ -2,10 +2,10 @@ from .parser import subparsers
 from argparse import Namespace
 
 
-def find_handler(args: Namespace):
+def search_handler(args: Namespace):
     pass
 
 
 # Find parser
-find_parser = subparsers.add_parser('save', help='command to find idea in saved ideas')
-find_parser.set_deffaults(func=find_handler)
+find_parser = subparsers.add_parser('search', help='command to find idea in saved ideas')
+find_parser.set_deffaults(func=search_handler)
