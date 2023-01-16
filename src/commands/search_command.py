26dfcd8ad003b796491af7639dd0742a1c75d843
Konstantin-create <87200874+Konstantin-create@users.noreply.github.com>
2023-01-16 11:01:08 +0300

    feat: approximate search

--- src/commands/search_command.py
+++ src/commands/search_command.py
@@ -6,17 +6,15 @@ class SearchCommand:
     def __init__(self):
         pass
 
-    def by_string(self, string_to_find: str) -> list:
+    def by_string(self, string_to_find: str) -> dict:
         """Function to find idea by part of header/data"""
 
         ideas = get_ideas_data()
-        ideas_out = []
+        ideas_out = {}
 
         for i in range(len(ideas)):
-            ideas_out.append({
-                i: max(compare_strings(needle=string_to_find, hay=ideas[i]['header']),
-                       compare_strings(needle=string_to_find, hay=ideas[i]['body']))
-            })
+            ideas_out[i] = max(compare_strings(needle=string_to_find, hay=ideas[i]['header']),
+                               compare_strings(needle=string_to_find, hay=ideas[i]['body']))
 
         return ideas_out
 
@@ -24,5 +22,11 @@ class SearchCommand:
         """Function to print search result of approximate search"""
 
         ideas_weights = self.by_string(string_to_find)
-        top_idea_id = sorted(ideas_weights)
-        print(top_idea_id)
+        top_idea_id = max(ideas_weights, key=lambda idea: ideas_weights[idea])
+
+        if ideas_weights[top_idea_id] == 0:
+            print('[red]Idea were n\'t found[/red]')
+            return
+        if ideas_weights[top_idea_id] <= 0.5:
+            print('[yellow]Similar ideas were n\'t found')
+        print(ideas_weights)
