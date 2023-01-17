6bd6eba972452bd91645943bc6fa53f445814603
Konstantin-create <87200874+Konstantin-create@users.noreply.github.com>
2023-01-17 15:37:38 +0300

    feat: load from file arg parser

--- src/parsers/save_parser.py
+++ src/parsers/save_parser.py
@@ -7,6 +7,10 @@ from commands import SaveCommand
 def save_handler(args: Namespace):
     """Handler for save command"""
 
+    if args.load_from_file:
+        # todo: load from file
+        return
+
     if args.add_list:
         print(f'[green]Adding {args.add_list} ideas[/green]')
         for i in range(args.add_list):
@@ -17,6 +21,7 @@ def save_handler(args: Namespace):
             save_obj.save_idea()
             print('[yellow]Idea were added[/yellow]')
         return
+
     header = input('    Idea header: ')
     body = input('    Idea body: ')
     save_obj = SaveCommand(header=header, body=body)
@@ -30,6 +35,13 @@ save_parser.add_argument(
     dest='add_list',
     type=int,
     help='command to save list of ideas'
+)
+
+save_parser.add_argument(
+    '-f', '--file',
+    dest='load_from_file',
+    action='store_true',
+    help='command to load ideas list from custom file'
 )
 
 save_parser.set_defaults(func=save_handler)
