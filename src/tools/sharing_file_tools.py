b6915f1f91dc7ade9a3981f334b5b2852a1ff9a2
Konstantin-create <87200874+Konstantin-create@users.noreply.github.com>
2023-01-18 09:31:13 +0300

    feat: .wtc files parser

--- src/tools/sharing_file_tools.py
+++ src/tools/sharing_file_tools.py
@@ -8,7 +8,19 @@ def parse_file_data(file_data: str) -> dict:
 
     for data_line in file_data:
         if '[PC_NAME]' in data_line:
-            pass
+            parsed_data['pc_name'] = data_line[data_line.find('[PC_NAME]') + 9:data_line.find('[/PC_NAME]')]
+        if '[PC_TIMESTAMP]' in data_line:
+            parsed_data['pc_timestamp'] = data_line[
+                                          data_line.find('[PC_TIMESTAMP]') + 14:data_line.find('[/PC_TIMESTAMP]')
+                                          ]
+        if '[IDEAS]' in data_line:
+            parsed_data['ideas'] = []
+            while '[/IDEAS]' not in data_line:
+                if '[IDEA]' in data_line:
+                    while '[/IDEA]' not in data_line:
+                        break
+
+    print(parsed_data)
 
 
 def load_wtc_file(path: str) -> dict:
@@ -19,3 +31,4 @@ def load_wtc_file(path: str) -> dict:
 
     with open(path, 'r') as file:
         file_data = file.readlines()
+    parse_file_data(file_data)
