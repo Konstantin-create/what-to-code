1aa1f50d9b2991036080865098113a1f6b26a40a
Konstantin-create <87200874+Konstantin-create@users.noreply.github.com>
2023-01-20 23:14:29 +0300

    feat: connect LoadCommand.show_ideas to load parser

--- src/parsers/load_parser.py
+++ src/parsers/load_parser.py
@@ -9,6 +9,8 @@ def load_handler(args: Namespace):
         print(f'[red]"{args.file_path.strip()}" - does n\'t exists[/red]')
         return
     load_command = LoadCommand(file_path=args.file_path.strip(), autosave=args.autosave)
+    if args.show_ideas:
+        load_command.show_ideas()
     load_command.by_path()
 
 
@@ -26,6 +28,13 @@ load_parser.add_argument(
     dest='autosave',
     action='store_true',
     help='auto save ideas from .wtc file'
+)
+
+load_parser.add_argumnt(
+    '-s', '--show',
+    dest='show_ideas',
+    action='store_true',
+    help='show list of ideas in file without saving'
 )
 
 load_parser.set_defaults(func=load_handler)
